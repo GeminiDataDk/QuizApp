a05507cca74c661885360e40d9f17a327b466f33
Daniyal Khan <daniyal_khan91@yahoo.com>
2023-02-13 23:05:12 +0500

    First commit

---
+++ src/Data/QuizData.js
@@ -0,0 +1,32 @@
+export const QuizData = [
+    {
+        question: "Html Stands For _______________________",
+        options:[ "Hyper Text Makeup Language","html","Case Cading Style Sheet","Hypertext markup language"],
+        answer: 4
+    },
+    {
+        question: "Css Stands For _______________________",
+        options:["Casecading Style Sheet","Java","Ram","Hypertext markup language"],
+        answer: 1
+    },
+    {
+        question: "Js Stands For _______________________",
+        options: ["Java Style","Java Script","Script","Script Src"],
+        answer: 2
+    },
+    {
+        question: "Dom Stands For _______________________",
+        options: [   "Document Object Model","html","Css","Java"],
+        answer:1
+    },
+    {
+        question:  "Ram Stands For _______________________",
+        options: [ "Read Only Memory","Dom","Random Acccess Memory","For Pc"],
+        answer:3
+    },
+    {
+        question:   "Rom Stands For _______________________",
+        options: [  "Hyper Text Markup Language","html","HTml","Read Only Memory"],
+        answer:4
+    },
+];
